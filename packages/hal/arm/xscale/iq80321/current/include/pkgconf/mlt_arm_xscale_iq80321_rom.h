62518ec560a9b0c20ec0bb895379eaa1cc0bd003
msalter <none@none>
2002-07-15 20:06:58 +0000

    IQ80321 mlt tweak

--- packages/hal/arm/xscale/iq80321/current/include/pkgconf/mlt_arm_xscale_iq80321_rom.h
+++ packages/hal/arm/xscale/iq80321/current/include/pkgconf/mlt_arm_xscale_iq80321_rom.h
@@ -1,4 +1,4 @@
-// eCos memory layout - Fri Oct 20 05:56:55 2000
+// eCos memory layout - Tue Jul 02 10:03:04 2002
 
 // This is a generated file - do not edit
 
@@ -10,11 +10,11 @@
 #define CYGMEM_REGION_ram (0)
 #define CYGMEM_REGION_ram_SIZE (0x2000000)
 #define CYGMEM_REGION_ram_ATTR (CYGMEM_REGION_ATTR_R | CYGMEM_REGION_ATTR_W)
-#define CYGMEM_REGION_rom (0xF0000000)
+#define CYGMEM_REGION_rom (0xf0000000)
 #define CYGMEM_REGION_rom_SIZE (0x40000)
 #define CYGMEM_REGION_rom_ATTR (CYGMEM_REGION_ATTR_R)
 #ifndef __ASSEMBLER__
 extern char CYG_LABEL_NAME (__heap1) [];
 #endif
 #define CYGMEM_SECTION_heap1 (CYG_LABEL_NAME (__heap1))
-#define CYGMEM_SECTION_heap1_SIZE (0x1e00000 - (size_t) CYG_LABEL_NAME (__heap1))
+#define CYGMEM_SECTION_heap1_SIZE (0x2000000 - (size_t) CYG_LABEL_NAME (__heap1))
