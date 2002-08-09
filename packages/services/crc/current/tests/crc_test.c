6dda0a3f97c15c28a510f47d4438133fb0da7dec
asl <none@none>
2002-08-09 10:27:03 +0000

    * Created a new package from the CRC routines           embedded in RedBoot.

---
+++ packages/services/crc/current/tests/crc_test.c
@@ -0,0 +1,100 @@
+//=================================================================
+//
+//        crc_test.c
+//
+//        crc test cases
+//
+//==========================================================================
+//####ECOSGPLCOPYRIGHTBEGIN####
+// -------------------------------------------
+// This file is part of eCos, the Embedded Configurable Operating System.
+// Copyright (C) 2002 Andrew Lunn
+//
+// eCos is free software; you can redistribute it and/or modify it under
+// the terms of the GNU General Public License as published by the Free
+// Software Foundation; either version 2 or (at your option) any later version.
+//
+// eCos is distributed in the hope that it will be useful, but WITHOUT ANY
+// WARRANTY; without even the implied warranty of MERCHANTABILITY or
+// FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+// for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with eCos; if not, write to the Free Software Foundation, Inc.,
+// 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.
+//
+// As a special exception, if other files instantiate templates or use macros
+// or inline functions from this file, or you compile this file and link it
+// with other works to produce a work based on this file, this file does not
+// by itself cause the resulting work to be covered by the GNU General Public
+// License. However the source code for this file must still be made available
+// in accordance with section (3) of the GNU General Public License.
+//
+// This exception does not invalidate any other reasons why a work based on
+// this file might be covered by the GNU General Public License.
+//
+// Alternative licenses for eCos may be arranged by contacting Red Hat, Inc.
+// at http://sources.redhat.com/ecos/ecos-license/
+// -------------------------------------------
+//####ECOSGPLCOPYRIGHTEND####
+//==========================================================================
+//#####DESCRIPTIONBEGIN####
+//
+// Author(s):     asl
+// Contributors:  asl
+// Date:          2002-08-06
+// Description:   Tests the calculation code
+//####DESCRIPTIONEND####
+
+#include <cyg/infra/testcase.h>
+#include <cyg/crc/crc.h>
+
+static char license_txt[] = 
+"		    GNU GENERAL PUBLIC LICENSE
+		       Version 2, June 1991
+
+ Copyright (C) 1989, 1991 Free Software Foundation, Inc.
+     59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ Everyone is permitted to copy and distribute verbatim copies
+ of this license document, but changing it is not allowed.
+
+			    Preamble
+
+  The licenses for most software are designed to take away your
+freedom to share and change it.  By contrast, the GNU General Public
+License is intended to guarantee your freedom to share and change free
+software--to make sure the software is free for all its users.  This
+General Public License applies to most of the Free Software
+Foundation's software and to any other program whose authors commit to
+using it.  (Some other Free Software Foundation software is covered by
+the GNU Library General Public License instead.)  You can apply it to
+your programs, too.";
+
+externC void
+cyg_start( void )
+{
+  CYG_TEST_INIT();
+  
+  CYG_TEST_INFO("Calculating CRCs");
+ 
+  if (1500790746l != cyg_posix_crc32(license_txt,sizeof(license_txt)-1)) {
+    CYG_TEST_FAIL("Wrong POSIX CRC32 calculation");
+  } else {
+    CYG_TEST_PASS("POSIX CRC32 calculation");
+  }
+  
+  if (1247800780 != cyg_crc32(license_txt,sizeof(license_txt)-1)) {
+    CYG_TEST_FAIL("Wrong Gary S. Browns' crc32 calculation");
+  } else {
+    CYG_TEST_PASS("Gary S. Browns' crc32 calculation");
+  }
+
+  if (32256 != cyg_crc16(license_txt,sizeof(license_txt)-1)) {
+    CYG_TEST_FAIL_FINISH("Wrong 16bit CRC calculation");
+  } else {
+    CYG_TEST_PASS_FINISH("16bit CRC calculation");
+  }
+}
+
+  
+
