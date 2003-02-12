948da4c278133e42599c99f45fa46dc7ac7d5066
msalter <none@none>
2003-02-12 14:01:14 +0000

    *** empty log message ***

--- packages/devs/eth/intel/i82559/current/src/if_i82559.c
+++ packages/devs/eth/intel/i82559/current/src/if_i82559.c
@@ -9,6 +9,7 @@
 // -------------------------------------------
 // This file is part of eCos, the Embedded Configurable Operating System.
 // Copyright (C) 1998, 1999, 2000, 2001, 2002, 2003 Red Hat, Inc.
+// Copyright (C) 2002, 2003 Gary Thomas
 //
 // eCos is free software; you can redistribute it and/or modify it under
 // the terms of the GNU General Public License as published by the Free
