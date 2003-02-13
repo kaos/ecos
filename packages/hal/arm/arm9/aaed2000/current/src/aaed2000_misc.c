0f1f0d9fbefc99f5ef4cb2cd7299fef7fcbdf982
jlarmour <none@none>
2003-02-13 19:10:10 +0000

    * src/aaed2000_misc.c: Replaced explicit prototype of memset with 	#include <string.h>.

--- packages/hal/arm/arm9/aaed2000/current/src/aaed2000_misc.c
+++ packages/hal/arm/arm9/aaed2000/current/src/aaed2000_misc.c
@@ -67,7 +67,7 @@
 
 #include <cyg/infra/diag.h>             // diag_printf
 
-externC void* memset(void *, int, size_t);
+#include <string.h> // memset
 
 
 // -------------------------------------------------------------------------
