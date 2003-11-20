b0f5eacd49dc07305019d9908fc2249704c022bf
asl <none@none>
2003-11-20 09:28:06 +0000

    * tests/zlib[12].c: Modified the text to the gcc3.3 friendly.         * Upgrade to zlib 1.1.4 and additional fix for inflate bug on         machines with 16-bit pointers.

--- packages/services/compress/zlib/current/include/zlib.h
+++ packages/services/compress/zlib/current/include/zlib.h
@@ -1,7 +1,7 @@
 /* zlib.h -- interface of the 'zlib' general purpose compression library
-  version 1.1.3, July 9th, 1998
+  version 1.1.4, March 11th, 2002
 
-  Copyright (C) 1995-1998 Jean-loup Gailly and Mark Adler
+  Copyright (C) 1995-2002 Jean-loup Gailly and Mark Adler
 
   This software is provided 'as-is', without any express or implied
   warranty.  In no event will the authors be held liable for any damages
@@ -37,7 +37,7 @@
 extern "C" {
 #endif
 
-#define ZLIB_VERSION "1.1.3"
+#define ZLIB_VERSION "1.1.4"
 
 /* 
      The 'zlib' compression library provides in-memory compression and
@@ -637,7 +637,6 @@ ZEXTERN int ZEXPORT compress2 OF((Bytef *dest,   uLongf *destLen,
 
 ZEXTERN int ZEXPORT uncompress OF((Bytef *dest,   uLongf *destLen,
                                    const Bytef *source, uLong sourceLen));
-
 /*
      Decompresses the source buffer into the destination buffer.  sourceLen is
    the byte length of the source buffer. Upon entry, destLen is the total
@@ -853,6 +852,7 @@ ZEXTERN uLong ZEXPORT crc32   OF((uLong crc, const Bytef *buf, uInt len));
      }
      if (crc != original_crc) error();
 */
+
 
                         /* various hacks, don't look :) */
 
--- packages/services/compress/zlib/current/tests/zlib2.c
+++ packages/services/compress/zlib/current/tests/zlib2.c
@@ -153,90 +153,89 @@ unsigned char gzip_test[] = {
 0xb5, 0xfa, 0x06, 0x64, 0xed, 0xaa, 0x07, 0x2f, 0x0c, 0x00};
 
 unsigned char gzip_test_ref[] =
-"Red Hat eCos Public License v1.1
-
-
-
-1. DEFINITIONS
-
-1.1. \"Contributor\" means each entity that creates or
-contributes to the creation of Modifications.
-
-1.2. \"Contributor Version\" means the combination of the
-Original Code, prior Modifications used by a
-Contributor, and the Modifications made by that
-particular Contributor.
-
-1.3. \"Covered Code\" means the Original Code or
-Modifications or the combination of the Original Code
-and Modifications, in each case including portions
-thereof.
-
-1.4. \"Electronic Distribution Mechanism\" means a
-mechanism generally accepted in the software development
-community for the electronic transfer of data.
-
-1.5. \"Executable\" means Covered Code in any form other
-than Source Code.
-1.6. \"Initial Developer\" means the individual or entity
-identified as the Initial Developer in the Source Code
-notice required by Exhibit A.
-
-1.7. \"Larger Work\" means a work which combines Covered
-Code or portions thereof with code not governed by the
-terms of this License.
-
-1.8. \"License\" means this document.
-
-1.9. \"Modifications\" means any addition to or deletion
-from the substance or structure of either the Original
-Code or any previous Modifications. When Covered Code is
-released as a series of files, a Modification is:
-
-     A. Any addition to or deletion from the
-     contents of a file containing Original Code or
-     previous Modifications.
-
-     B. Any new file that contains any part of the
-     Original Code or previous Modifications.
-
-1.10. \"Original Code\" means Source Code of computer
-software code which is described in the Source Code
-notice required by Exhibit A as Original Code, and
-which, at the time of its release under this License is
-not already Covered Code governed by this License.
-
-1.11. \"Source Code\" means the preferred form of the
-Covered Code for making modifications to it, including
-all modules it contains, plus any associated interface
-definition files, scripts used to control compilation
-and installation of an Executable, or a list of source
-code differential comparisons against either the
-Original Code or another well known, available Covered
-Code of the Contributor's choice. The Source Code can be
-in a compressed or archival form, provided the
-appropriate decompression or de-archiving software is
-widely available for no charge.
-
-1.12. \"You\" means an individual or a legal entity
-exercising rights under, and complying with all of the
-terms of, this License or a future version of this
-License issued under Section 6.1. For legal entities,
-\"You\" includes any entity which controls, is controlled
-by, or is under common control with You. For purposes of
-this definition, \"control\" means (a) the power, direct
-or indirect, to cause the direction or management of
-such entity, whether by contract or otherwise, or (b)
-ownership of fifty percent (50%) or more of the
-outstanding shares or beneficial ownership of such
-entity.
-
-1.13. \"Red Hat Branded Code\" is code that Red Hat
-distributes and/or permits others to distribute under
-different terms than the Red Hat eCos Public License.
-Red Hat's Branded Code may contain part or all of the
-Covered Code.
-";
+"Red Hat eCos Public License v1.1\n"
+"\n"
+"\n"
+"\n"
+"1. DEFINITIONS\n"
+"\n"
+"1.1. \"Contributor\" means each entity that creates or\n"
+"contributes to the creation of Modifications.\n"
+"\n"
+"1.2. \"Contributor Version\" means the combination of the\n"
+"Original Code, prior Modifications used by a\n"
+"Contributor, and the Modifications made by that\n"
+"particular Contributor.\n"
+"\n"
+"1.3. \"Covered Code\" means the Original Code or\n"
+"Modifications or the combination of the Original Code\n"
+"and Modifications, in each case including portions\n"
+"thereof.\n"
+"\n"
+"1.4. \"Electronic Distribution Mechanism\" means a\n"
+"mechanism generally accepted in the software development\n"
+"community for the electronic transfer of data.\n"
+"\n"
+"1.5. \"Executable\" means Covered Code in any form other\n"
+"than Source Code.\n"
+"1.6. \"Initial Developer\" means the individual or entity\n"
+"identified as the Initial Developer in the Source Code\n"
+"notice required by Exhibit A.\n"
+"\n"
+"1.7. \"Larger Work\" means a work which combines Covered\n"
+"Code or portions thereof with code not governed by the\n"
+"terms of this License.\n"
+"\n"
+"1.8. \"License\" means this document.\n"
+"\n"
+"1.9. \"Modifications\" means any addition to or deletion\n"
+"from the substance or structure of either the Original\n"
+"Code or any previous Modifications. When Covered Code is\n"
+"released as a series of files, a Modification is:\n"
+"\n"
+"     A. Any addition to or deletion from the\n"
+"     contents of a file containing Original Code or\n"
+"     previous Modifications.\n"
+"\n"
+"     B. Any new file that contains any part of the\n"
+"     Original Code or previous Modifications.\n"
+"\n"
+"1.10. \"Original Code\" means Source Code of computer\n"
+"software code which is described in the Source Code\n"
+"notice required by Exhibit A as Original Code, and\n"
+"which, at the time of its release under this License is\n"
+"not already Covered Code governed by this License.\n"
+"\n"
+"1.11. \"Source Code\" means the preferred form of the\n"
+"Covered Code for making modifications to it, including\n"
+"all modules it contains, plus any associated interface\n"
+"definition files, scripts used to control compilation\n"
+"and installation of an Executable, or a list of source\n"
+"code differential comparisons against either the\n"
+"Original Code or another well known, available Covered\n"
+"Code of the Contributor's choice. The Source Code can be\n"
+"in a compressed or archival form, provided the\n"
+"appropriate decompression or de-archiving software is\n"
+"widely available for no charge.\n"
+"\n"
+"1.12. \"You\" means an individual or a legal entity\n"
+"exercising rights under, and complying with all of the\n"
+"terms of, this License or a future version of this\n"
+"License issued under Section 6.1. For legal entities,\n"
+"\"You\" includes any entity which controls, is controlled\n"
+"by, or is under common control with You. For purposes of\n"
+"this definition, \"control\" means (a) the power, direct\n"
+"or indirect, to cause the direction or management of\n"
+"such entity, whether by contract or otherwise, or (b)\n"
+"ownership of fifty percent (50%) or more of the\n"
+"outstanding shares or beneficial ownership of such\n"
+"entity.\n"
+"\n"
+"1.13. \"Red Hat Branded Code\" is code that Red Hat\n"
+"distributes and/or permits others to distribute under\n"
+"different terms than the Red Hat eCos Public License.\n"
+"Red Hat's Branded Code may contain part or all of the\n"
+"Covered Code.\n";
 
 static void entry0( cyg_addrword_t data )
 {
