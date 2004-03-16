3952adedf655c3e05cbea82ab185cafa9ea1afca
asl <none@none>
2004-03-16 12:42:05 +0000

    * src/fs-ecos.c:         With CYGOPT_FS_JFFS2_WRITE=1, file creation failed. The problem         was introduced in fs-ecos.c 1.20

--- packages/fs/jffs2/current/src/fs-ecos.c
+++ packages/fs/jffs2/current/src/fs-ecos.c
@@ -686,7 +686,7 @@ static int jffs2_open(cyg_mtab_entry * mte, cyg_dir dir, const char *name,
 	if (!dir)
 		dir = mte->root;
 
-#ifdef CYGOPT_FS_JFFS2_WRITE
+#ifndef CYGOPT_FS_JFFS2_WRITE
 	if (mode & (O_CREAT|O_TRUNC|O_WRONLY))
 		return EROFS;
 #endif
