58f9c42e8d89e6823b04fed19f0b2dfb8626f662
jlarmour <none@none>
2005-04-14 16:42:12 +0000

    * src/common/stream.cxx (read): don't reference flags.buffering         when configured by CDL to be unbuffered.

--- packages/language/c/libc/stdio/current/src/common/stream.cxx
+++ packages/language/c/libc/stdio/current/src/common/stream.cxx
@@ -394,7 +394,11 @@ Cyg_StdioStream::read( cyg_uint8 *user_buffer, cyg_ucount32 buffer_length,
     //
     // unless we do this, we could end up reading byte-by-byte from the filing system
     // due to the readbuf_char scheme.
-    if (!flags.buffering && (*bytes_read<buffer_length)) {
+    if (
+#ifdef CYGSEM_LIBC_STDIO_WANT_BUFFERED_IO
+        !flags.buffering &&
+#endif
+        (*bytes_read<buffer_length)) {
         cyg_uint32 len;
         len=buffer_length-*bytes_read;
         read_err = cyg_stdio_read(my_device, user_buffer + *bytes_read, &len);      
