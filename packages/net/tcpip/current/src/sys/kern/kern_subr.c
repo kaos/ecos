342cdb4157a012856623a5fc22a589cdcadf127b
asl <none@none>
2005-10-23 20:49:28 +0000

    * src/sys/kern/kern_subr.c (uiomove): Remove lvalue cast to         keep GCC4.x happy.

--- packages/net/tcpip/current/src/sys/kern/kern_subr.c
+++ packages/net/tcpip/current/src/sys/kern/kern_subr.c
@@ -134,7 +134,7 @@ uiomove(cp, n, uio)
 			break;
 #endif
 		}
-		(char *)(iov->iov_base) += cnt;
+                iov->iov_base = (char *)iov->iov_base + cnt;
 		iov->iov_len -= cnt;
 		uio->uio_resid -= cnt;
 		uio->uio_offset += cnt;
