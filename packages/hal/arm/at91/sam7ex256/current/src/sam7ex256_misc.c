60815570e15c412e0c3ba48549aecc3009f18f45
asl <none@none>
2007-01-07 15:22:05 +0000

    SAM7-EX256 development board package

---
+++ packages/hal/arm/at91/sam7ex256/current/src/sam7ex256_misc.c
@@ -0,0 +1,69 @@
+/*==========================================================================
+//
+//      sam7ex256_misc.c
+//
+//      HAL misc board support code for Olimex SAM7-EX256 board
+//
+//==========================================================================
+//####ECOSGPLCOPYRIGHTBEGIN####
+// -------------------------------------------
+// This file is part of eCos, the Embedded Configurable Operating System.
+// Copyright (C) 2006 Andrew Lunn <andrew.lunn@ascom.ch>
+//
+// eCos is free software; you can redistribute it and/or modify it under
+// the terms of the GNU General Public License as published by the Free
+// Software Foundation; either version 2 or (at your option) any later version.
+//
+// eCos is distributed in the hope that it will be useful, but WITHOUT ANY
+// WARRANTY; without even the implied warranty of MERCHANTABILITY or
+// FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+// for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with eCos; if not, write to the Free Software Foundation, Inc.,
+// 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.
+//
+// As a special exception, if other files instantiate templates or use macros
+// or inline functions from this file, or you compile this file and link it
+// with other works to produce a work based on this file, this file does not
+// by itself cause the resulting work to be covered by the GNU General Public
+// License. However the source code for this file must still be made available
+// in accordance with section (3) of the GNU General Public License.
+//
+// This exception does not invalidate any other reasons why a work based on
+// this file might be covered by the GNU General Public License.
+// -------------------------------------------
+//####ECOSGPLCOPYRIGHTEND####
+//==========================================================================
+//#####DESCRIPTIONBEGIN####
+//
+// Author(s):    Uwe Kindler
+// Contributors: Andrew Lunn, John Eigelaar
+// Date:         2006-12-30
+// Purpose:      HAL board support
+// Description:  Implementations of HAL board interfaces
+//
+//####DESCRIPTIONEND####
+//
+//========================================================================*/
+
+#include <pkgconf/system.h>
+#include <pkgconf/hal.h>
+
+#include <cyg/hal/hal_io.h>             // IO macros
+
+//
+// the development board does not contain any leds but this function is
+// required for proper compilation - maybe we can use the integrated
+// nokia 320x 320 pixel lcd for led debug output
+//
+void hal_at91_led (int val)
+{
+    //
+    // todo: implement led simulation for nokia 320 x 320 pixel LCD
+    //
+}
+
+
+//--------------------------------------------------------------------------
+// EOF sam7ex256_misc.c
