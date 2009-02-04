1de5f186ddab0a9dc6f1d93b5294fb4e896a1fe1
bartv <none@none>
2009-02-04 18:19:29 +0000

    Switch to CYG_INIT_INTERRUPTS priority.

--- packages/devs/watchdog/arm/at91wdtc/current/src/watchdog_at91wdtc.cxx
+++ packages/devs/watchdog/arm/at91wdtc/current/src/watchdog_at91wdtc.cxx
@@ -8,7 +8,7 @@
 // ####ECOSGPLCOPYRIGHTBEGIN####                                            
 // -------------------------------------------                              
 // This file is part of eCos, the Embedded Configurable Operating System.   
-// Copyright (C) 1998, 1999, 2000, 2001, 2002, 2003, 2006 Free Software Foundation, Inc.
+// Copyright (C) 1998, 1999, 2000, 2001, 2002, 2003, 2006, 2009 Free Software Foundation, Inc.
 //
 // eCos is free software; you can redistribute it and/or modify it under    
 // the terms of the GNU General Public License as published by the Free     
@@ -117,7 +117,7 @@ isr(cyg_vector vector, CYG_ADDRWORD data)
 
 //==========================================================================
 
-static CYGBLD_ATTRIB_INIT_PRI(CYG_INIT_DRIVERS)
+static CYGBLD_ATTRIB_INIT_PRI(CYG_INIT_INTERRUPTS)
   Cyg_Interrupt wdint(
                       CYGNUM_HAL_INTERRUPT_WDTC,
                       INT_PRIO,
