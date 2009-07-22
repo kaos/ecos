1dbd3a980e9129287f7a129d00a506ec0d1c01c0
jld <none@none>
2009-07-22 19:17:14 +0000

    * include/sys/cdefs.h: Silence undefined macro warning reported by Jay Foster.

--- packages/net/bsd_tcpip/current/include/sys/cdefs.h
+++ packages/net/bsd_tcpip/current/include/sys/cdefs.h
@@ -159,7 +159,7 @@
 #endif
 
 /* Compiler-dependent macros that rely on FreeBSD-specific extensions. */
-#if __FreeBSD_cc_version >= 300001
+#if defined(__FreeBSD_cc_version) && (__FreeBSD_cc_version >= 300001)
 #define	__printf0like(fmtarg, firstvararg) \
 	    __attribute__((__format__ (__printf0__, fmtarg, firstvararg)))
 #else
